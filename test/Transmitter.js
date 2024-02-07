33cda2bc97f2ef5ac2cc5744e4ea9d0e2ba6e032
Maanasa_Rao <140826809+maanasarao01@users.noreply.github.com>
2024-02-07 21:57:38 +0530

    Update Transmitter.js

--- test/Transmitter.js
+++ test/Transmitter.js
@@ -1,4 +1,4 @@
-const {networkTransmitStub, transmitInCelcius}=require( "../transmitter.js");
+import {networkTransmitStub, transmitInCelcius} from "../transmitter.js";
 
 transmitInCelcius(400.5);
 transmitInCelcius(303.6);
