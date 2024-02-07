b192891637de7e38a077a0d0b9fdba7c48bfadb5
Maanasa_Rao <140826809+maanasarao01@users.noreply.github.com>
2024-02-07 18:38:38 +0530

    Create tshirt.js

---
+++ test/tshirt.js
@@ -0,0 +1,8 @@
+const {size}=require("./tshirts.js");
+const {expect} = require('chai');
+
+expect(size(37)).equals('S');
+expect(size(40)).equals('M');
+expect(size(43)).equals('L');
+expect(size(38)).equals('M');//this should fail here as the function returns 'L'
+console.log('All is well (maybe!)');
