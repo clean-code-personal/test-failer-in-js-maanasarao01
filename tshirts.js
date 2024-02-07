5f3dfb15939af4f35cd237022b987c91c43c6cfc
Maanasa_Rao <140826809+maanasarao01@users.noreply.github.com>
2024-02-07 18:22:13 +0530

    Update tshirts.js

--- tshirts.js
+++ tshirts.js
@@ -13,5 +13,5 @@ function size(cms) {
 expect(size(37)).equals('S');
 expect(size(40)).equals('M');
 expect(size(43)).equals('L');
-expect(size(38)).equals('M');
+expect(size(38)).equals('M');//this should fail here as the function returns 'L'
 console.log('All is well (maybe!)');
