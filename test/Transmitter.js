02cc674adb61d355935827f96a7c5511b2905087
Maanasa_Rao <140826809+maanasarao01@users.noreply.github.com>
2024-02-07 21:37:09 +0530

    Create Transmitter.js

---
+++ test/Transmitter.js
@@ -0,0 +1,13 @@
+import {transmitInCelcius} from "./transmitter.js";
+
+transmitInCelcius(400.5);
+transmitInCelcius(303.6);
+
+networkTransmitStub=function(celsius){
+    //non-ok response to simulate failure
+    console.log(`Simulating Failue! Temperature to transmit: ${celcius} celcius`);
+    return 500;
+}
+transmitInCelcius(300);//this sould fail 
+console.log(`Transmission failed ${transmissionFailureCount} times.`);
+//console.log('All is well (maybe!)');
