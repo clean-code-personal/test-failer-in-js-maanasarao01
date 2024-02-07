23ba838675eff81b5b97c57fc4fa1514724e27bb
Maanasa_Rao <140826809+maanasarao01@users.noreply.github.com>
2024-02-07 21:33:22 +0530

    Update transmitter.js

--- transmitter.js
+++ transmitter.js
@@ -13,7 +13,7 @@ function networkTransmitStub(celcius) {
     return 200;
 }
 
-function transmitInCelcius(farenheit) {
+export function transmitInCelcius(farenheit) {
     const celcius = (farenheit - 32) * 5 / 9;
     const returnCode = networkTransmitStub(celcius);
     if (returnCode != 200) {
@@ -24,15 +24,3 @@ function transmitInCelcius(farenheit) {
         transmissionFailureCount += 0;
     }
 }
-
-transmitInCelcius(400.5);
-transmitInCelcius(303.6);
-
-networkTransmitStub=function(celsius){
-    //non-ok response to simulate failure
-    console.log(`Simulating Failue! Temperature to transmit: ${celcius} celcius`);
-    return 500;
-}
-transmitInCelcius(300);//this sould fail 
-console.log(`Transmission failed ${transmissionFailureCount} times.`);
-//console.log('All is well (maybe!)');
