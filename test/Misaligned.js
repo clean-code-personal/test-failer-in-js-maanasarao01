e2e9c04fdcd8900df0bc1c50c225f238fbb2caeb
Maanasa_Rao <140826809+maanasarao01@users.noreply.github.com>
2024-02-08 12:01:55 +0530

    Update Misaligned.js

--- test/Misaligned.js
+++ test/Misaligned.js
@@ -3,7 +3,7 @@ const {color_map,print_color_map} =require("../misaligned.js");
 
 const loggedOutput= print_color_map(color_map());
 
-loggedOutput.forEach(colorCodes){
+loggedOutput.forEach(colorCodes)=>{
   let colorParts=colorCodes.split('|');
   expect(colorParts[0].length).equals(3);//this should fail if PairNumber is misaligned
   expect(colorParts[1].length).equals(7);//this should fail if the colorCodes are misaligned
