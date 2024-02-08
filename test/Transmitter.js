7fe43c527ce2a56125bda5c9b89a693872d56bab
Maanasa_Rao <140826809+maanasarao01@users.noreply.github.com>
2024-02-08 19:50:37 +0530

    Update Transmitter.js

--- test/Transmitter.js
+++ test/Transmitter.js
@@ -1,13 +1,31 @@
 const {expect}=require('chai');
 const { transmitInCelcius, transmissionFailureCount} =require('../transmitter.js');
 
-transmitInCelcius(400.5);
-transmitInCelcius(303.6);
+let flag=1;
+function networkTransmitStub(celcius) {
+    console.log(`Temperature to transmit: ${celcius} celcius`);
+    const roundedNumber = Math.round(Math.random());
+    const stubCode;
+    if(flag){
+      flag=0;
+      return 500;
+    }
+    else{
+      // Map 0 to 200 and 1 to 500
+      // This stub either returns 200 or 500
+      stubCode = (roundedNumber == 0) ? 200 : 500;
+      return stubCode;
+    }
+}
 
-transmitInCelcius(300); 
-transmitInCelcius(400);
+//using dependency injection
+transmitInCelcius(400.5,networkTransmitStub);
+transmitInCelcius(303.6,networkTransmitStub);
 
-transmitInCelcius(304.9); 
+transmitInCelcius(300,networkTransmitStub); 
+transmitInCelcius(400,networkTransmitStub);
+
+transmitInCelcius(304.9,networkTransmitStub); 
 console.log(`Transmission failed ${transmissionFailureCount} times.`);
 expect(transmissionFailureCount).to.be.at.least(1);
 console.log('All is well (maybe!)');
