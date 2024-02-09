83709aeada7115825776590db0f05eae7f743731
Maanasa_Rao <140826809+maanasarao01@users.noreply.github.com>
2024-02-09 08:01:46 +0530

    Update misaligned.js

--- misaligned.js
+++ misaligned.js
@@ -1,9 +1,10 @@
+const colorslength=5;
 function color_map() {
     const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
     const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
     let colorCodes=[];
-    for (let i = 0; i < majorColors.length; i++) {
-        for (let j = 0; j < minorColors.length; j++) {
+    for (let i = 0; i < colorsLength; i++) {
+        for (let j = 0; j < colorsLength; j++) {
             //to seperate the formatting from console.log
             colorCodes.push(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`);
         }
@@ -18,4 +19,4 @@ function color_map() {
     });
     
 
-module.exports= {color_map};
+module.exports= {color_map,colorsLength};
