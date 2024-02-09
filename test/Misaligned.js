0cc473e4017586ed88b084847c77b6af34f2f219
Maanasa_Rao <140826809+maanasarao01@users.noreply.github.com>
2024-02-09 08:07:57 +0530

    Update Misaligned.js

--- test/Misaligned.js
+++ test/Misaligned.js
@@ -2,7 +2,6 @@ const {expect}=require('chai');
 const {color_map} =require("../misaligned.js");
 
 const loggedOutput= color_map();
-
 // Check alignment of each parameter
 let pairNumberLength = null;
 let majorColorLength = null;
@@ -13,18 +12,19 @@ loggedOutput.forEach(colorCodes => {
   let pairNumber = colorParts[0]; 
   let majorColor = colorParts[1];
   let minorColor = colorParts[2];
-
+  
   // If lengths are not initialized, set them to the length of the first color code
   if (pairNumberLength === null) {
     pairNumberLength = pairNumber.length;
     majorColorLength = majorColor.length;
     minorColorLength = minorColor.length;
   }
-
   // Check if lengths are consistent across all color codes
   expect(pairNumber.length).to.equal(pairNumberLength);
   expect(majorColor.length).to.equal(majorColorLength);
   expect(minorColor.length).to.equal(minorColorLength);
+  //Range check
+  expect(pairNumber).to.be.within(1,25);
 });
 
 // Check total color combinations
