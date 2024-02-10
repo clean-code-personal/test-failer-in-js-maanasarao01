f37bd4131a2a23123ef836397f62c0749d18f8c7
Maanasa_Rao <140826809+maanasarao01@users.noreply.github.com>
2024-02-10 09:30:45 +0530

    Update tshirt.js

--- test/tshirt.js
+++ test/tshirt.js
@@ -4,5 +4,5 @@ const {expect} = require('chai');
 expect(size(37)).equals('S');
 expect(size(40)).equals('M');
 expect(size(43)).equals('L');
-expect(size(38)).equals('M');//this should fail here as the function returns 'L'
+expect(size(38)).equals('S');//this should pass now 
 console.log('All is well (maybe!)');
