3e1196953b5fd43e0ca303352b9d9441bd81b337
Maanasa_Rao <140826809+maanasarao01@users.noreply.github.com>
2024-02-10 12:28:23 +0530

    Update Misaligned.js

--- test/Misaligned.js
+++ test/Misaligned.js
@@ -3,9 +3,10 @@ const {color_map} =require("../misaligned.js");
 
 const loggedOutput= color_map();
 // Check alignment of each parameter
-let pairNumberLength = null;
-let majorColorLength = null;
-let minorColorLength = null;
+//initially it will be unset
+let setLengthBasedOnFirstRow=0
+
+let saveEarlierPairNumber=-1
 
 loggedOutput.forEach(colorCodes => {
   let colorParts = colorCodes.split('|');
@@ -14,17 +15,18 @@ loggedOutput.forEach(colorCodes => {
   let minorColor = colorParts[2];
   
   // If lengths are not initialized, set them to the length of the first color code
-  if (pairNumberLength === null) {
-    pairNumberLength = pairNumber.length;
-    majorColorLength = majorColor.length;
-    minorColorLength = minorColor.length;
-  }
+  if (!setLengthBasedOnFirstRow) 
+    setLengthBasedOnFirstRow=1
+  
   // Check if lengths are consistent across all color codes
   expect(pairNumber.length).to.equal(pairNumberLength);
   expect(majorColor.length).to.equal(majorColorLength);
   expect(minorColor.length).to.equal(minorColorLength);
   //Range check
   expect(Number(pairNumber)).to.be.within(1,25);
+  //check for duplicates
+  expect((saveEarlierPairNumber).equals(pairNumber-1))
+  saveEarlierPairNumber=pairNumber
 });
 
 // Check total color combinations
