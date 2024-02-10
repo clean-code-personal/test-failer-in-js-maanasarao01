0f0ec864c1ddd3e8da5c7642d98fd7af849c6d71
Maanasa_Rao <140826809+maanasarao01@users.noreply.github.com>
2024-02-10 12:01:39 +0530

    Update misaligned.js

--- misaligned.js
+++ misaligned.js
@@ -3,12 +3,19 @@ function color_map() {
     const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
     const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
     let colorCodes=[];
-    for (let i = 0; i < majorColors.length; i++) {
-        for (let j = 0; j < minorColors.length; j++) {
-            //to seperate the formatting from console.log
-            colorCodes.push(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`);
+    const maxPairNumberLength = (majorColors.length * minorColors.length).toString().length;
+    const maxMajorColorLength = Math.max(...majorColors.map(color => color.length));
+    const maxMinorColorLength = Math.max(...minorColors.map(color => color.length));
+    
+        for (let i = 0; i < majorColors.length; i++) {
+            for (let j = 0; j < minorColors.length; j++) {
+                const pairNumber = i * 5 + j;
+                const formattedPairNumber = pairNumber.toString().padEnd(maxPairNumberLength, ' ');
+                const formattedMajorColor = majorColors[i].padEnd(maxMajorColorLength, ' ');
+                const formattedMinorColor = minorColors[j].padEnd(maxMinorColorLength, ' ');
+                colorCodes.push(`${formattedPairNumber} | ${formattedMajorColor} | ${formattedMinorColor}`);
+            }
         }
-    }
     return colorCodes;
 }
 
